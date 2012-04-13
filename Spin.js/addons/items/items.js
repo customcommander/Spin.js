e1d6b8c624157469e2a0f862d2336d5fce3762f6
Jonathan Sanchez <jonathansanchez@me.com>
2012-04-13 10:34:37 +0200

    use spin.js has submodule

--- Spin.js/addons/items/items.js
+++
@@ -1,7 +0,0 @@
-$(function (){
-    
-    $(document.body).delegate('li.spin-item:not([class~="loaded"])', 'mouseover mouseout', function (e){            
-        $(this).toggleClass('mouseover');            
-    });
-    
-});
